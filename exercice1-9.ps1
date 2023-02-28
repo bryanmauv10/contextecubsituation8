3627beb859b1675675391b73ce33f0e805776a60
bryanmauv10 <bryanmauv@gmail.com>
2023-02-28 11:23:25 +0100

    exercice1-9

---
+++ exercice1-9.ps1
@@ -0,0 +1,9 @@
+# Importer la liste des services démarrés depuis services.csv
+$services = Import-Csv -Path "services.csv" | Where-Object { $_.Status -eq "Running" }
+
+# Parcourir la liste des services et afficher le nom et le statut de chaque service contenant "system"
+foreach ($service in $services) {
+    if ($service.Name -match "system") {
+        Write-Host "$($service.Name) $($service.Status)" -ForegroundColor Green
+    }
+}
